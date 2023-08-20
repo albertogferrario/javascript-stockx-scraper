f73d4fb8bd13c6bd8eef0b38cb0bd68429605c4f
Alberto Giancarlo Ferrario <me@albertogferrar.io>
2023-08-20 10:45:29 +0700

    fix

--- src/entities/Product.js
+++ src/entities/Product.js
@@ -3,5 +3,5 @@ import Entity from '../Entity';
 export default class Product extends Entity {
   static parseResponse = (response) => super.parseResponse(response).data.product.variants;
 
-  getVariants = async (productSlug) => Product.parseResponse(this.client.get(`/${productSlug}`));
+  getVariants = async (productSlug) => Product.parseResponse(await this.client.get(`/${productSlug}`));
 }
